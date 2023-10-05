bcbb230833e938c9502b37bfacaf8377b81bb689
Dan Torres <danstorre@gmail.com>
2023-10-04 20:51:19 -0400

    on one element array delivers one element ordered array.

--- Algorithms-BasicsTests/SelectionSortTests.swift
+++ Algorithms-BasicsTests/SelectionSortTests.swift
@@ -3,14 +3,15 @@ import XCTest
 
 enum SelectionSort {
     static func sort(_ array: [Int]) -> [Int] {
-        []
+        array
     }
 }
 
 final class SelectionSortTests: XCTestCase {
     func test_sort_shouldDeliverSortedArray() throws {
         let samples: [(unsortedArray: [Int], expected: [Int])] = [
-            (unsortedArray: [], expected: [])
+            (unsortedArray: [], expected: []),
+            (unsortedArray: [0], expected: [0]),
         ]
         samples.forEach { (unsortedArray, expectedArray) in
             let sut: (([Int]) -> [Int]) = SelectionSort.sort
