49160fbd093ea8bc1736023ac1f53f19f9aff3d2
Dan Torres <danstorre@gmail.com>
2023-10-04 21:03:42 -0400

    sorts an array of two elements

--- Algorithms-BasicsTests/SelectionSortTests.swift
+++ Algorithms-BasicsTests/SelectionSortTests.swift
@@ -3,7 +3,26 @@ import XCTest
 
 enum SelectionSort {
     static func sort(_ array: [Int]) -> [Int] {
-        array
+        var originalCopy = array
+        var newArray = [Int]()
+        
+        for _ in 0..<originalCopy.count {
+            let smallestIndex = Self.findSmallest(originalCopy)
+            newArray.append(array[smallestIndex])
+            originalCopy.remove(at: smallestIndex)
+        }
+        
+        return newArray
+    }
+    
+    static func findSmallest(_ array: [Int]) -> Int {
+        var smallestIndex = 0
+        for i in 1..<array.count {
+            if array[i] < array[smallestIndex] {
+                smallestIndex = i
+            }
+        }
+        return smallestIndex
     }
 }
 
@@ -12,6 +31,7 @@ final class SelectionSortTests: XCTestCase {
         let samples: [(unsortedArray: [Int], expected: [Int])] = [
             (unsortedArray: [], expected: []),
             (unsortedArray: [0], expected: [0]),
+            (unsortedArray: [1,0], expected: [0,1]),
         ]
         samples.forEach { (unsortedArray, expectedArray) in
             let sut: (([Int]) -> [Int]) = SelectionSort.sort
