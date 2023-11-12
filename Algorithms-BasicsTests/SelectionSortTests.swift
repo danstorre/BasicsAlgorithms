e2369f02833f13117be67604e38d261a758abc43
Dan Torres <danstorre@gmail.com>
2023-11-12 18:56:30 -0400

    delivers one single element on one single element input array.

--- Algorithms-BasicsTests/SelectionSortTests.swift
+++ Algorithms-BasicsTests/SelectionSortTests.swift
@@ -11,7 +11,7 @@ final class SelectionSortTests: XCTestCase {
     func test_sort_shouldDeliverSortedArray() throws {
         let samples: [(unsortedArray: [Int], expected: [Int])] = [
             (unsortedArray: [], expected: []),
-//            (unsortedArray: [0], expected: [0]),
+            (unsortedArray: [0], expected: [0]),
 //            (unsortedArray: [1,0], expected: [0,1]),
 //            (unsortedArray: [3,2,1,0], expected: [0,1,2,3]),
         ]
