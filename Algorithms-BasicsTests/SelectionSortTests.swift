a658beb63e42ec205d887640232d417c817337ac
Dan Torres <danstorre@gmail.com>
2023-11-12 18:55:16 -0400

    starts sort selection exercise.

--- Algorithms-BasicsTests/SelectionSortTests.swift
+++ Algorithms-BasicsTests/SelectionSortTests.swift
@@ -3,19 +3,6 @@ import XCTest
 
 enum SelectionSort {
     static func sort(_ array: inout [Int]) {
-        let n = array.count
-        for i in 0..<n {
-            var minIndex = i
-            for j in i+1..<n {
-                if array[j] < array[minIndex] {
-                    minIndex = j
-                }
-            }
-            
-            if minIndex != i {            
-                array.swapAt(minIndex, i)
-            }
-        }
     }
 }
 
@@ -23,9 +10,9 @@ final class SelectionSortTests: XCTestCase {
     func test_sort_shouldDeliverSortedArray() throws {
         let samples: [(unsortedArray: [Int], expected: [Int])] = [
             (unsortedArray: [], expected: []),
-            (unsortedArray: [0], expected: [0]),
-            (unsortedArray: [1,0], expected: [0,1]),
-            (unsortedArray: [3,2,1,0], expected: [0,1,2,3]),
+//            (unsortedArray: [0], expected: [0]),
+//            (unsortedArray: [1,0], expected: [0,1]),
+//            (unsortedArray: [3,2,1,0], expected: [0,1,2,3]),
         ]
         samples.forEach { (unsortedArray, expectedArray) in
             var unsortedArray = unsortedArray
