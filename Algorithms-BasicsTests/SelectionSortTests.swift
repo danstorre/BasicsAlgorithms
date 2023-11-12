abee17170e8afca8eb555fb9c72786a84e7e39f0
Dan Torres <danstorre@gmail.com>
2023-11-12 19:37:23 -0400

    delivers correct sorted array on two items input.

--- Algorithms-BasicsTests/SelectionSortTests.swift
+++ Algorithms-BasicsTests/SelectionSortTests.swift
@@ -3,7 +3,19 @@ import XCTest
 
 enum SelectionSort {
     static func sort(_ array: inout [Int]) {
+        guard array.count > 0 else { return }
         
+        for i in 0..<array.count {
+            var smallestIndex = i
+            var smallesValue = array[smallestIndex]
+            for j in i+1..<array.count {
+                if array[j] < smallesValue {
+                    smallestIndex = j
+                    smallesValue = array[smallestIndex]
+                    array.swapAt(i, j)
+                }
+            }
+        }
     }
 }
 
@@ -12,7 +24,7 @@ final class SelectionSortTests: XCTestCase {
         let samples: [(unsortedArray: [Int], expected: [Int])] = [
             (unsortedArray: [], expected: []),
             (unsortedArray: [0], expected: [0]),
-//            (unsortedArray: [1,0], expected: [0,1]),
+            (unsortedArray: [1,0], expected: [0,1]),
 //            (unsortedArray: [3,2,1,0], expected: [0,1,2,3]),
         ]
         samples.forEach { (unsortedArray, expectedArray) in
